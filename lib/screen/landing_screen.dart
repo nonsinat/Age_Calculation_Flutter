da2fc92b44e12b627386052f4a6f6309f8ab45f8
NonSinat <sinat.non@student.niptict.edu.kh>
2021-09-08 15:20:49 +0700

    close master

--- lib/screen/landing_screen.dart
+++ lib/screen/landing_screen.dart
@@ -1,4 +1,7 @@
+import 'package:age_calculator_flutter/screen/home_screen.dart';
+import 'package:double_back_to_close_app/double_back_to_close_app.dart';
 import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
 
 class LandingScreen extends StatelessWidget {
   const LandingScreen({Key? key}) : super(key: key);
@@ -7,38 +10,54 @@ class LandingScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Container(
-        alignment: Alignment.center,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            TextButton(
-              onPressed: () {
-                print("Khmer");
-                
-              },
-              child: Text(
-                "ខ្មែរ",
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.white,
+      body: DoubleBackToCloseApp(
+        snackBar: SnackBar(
+          content: Text('Tap back again to leave'),
+        ),
+        child: Container(
+          alignment: Alignment.center,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              TextButton(
+                onPressed: () {
+                  print("Khmer");
+                  Navigator.push(
+                    context,
+                    PageTransition(
+                        child: HomeScreen(),
+                        type: PageTransitionType.leftToRight),
+                  );
+                },
+                child: Text(
+                  "ខ្មែរ",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
                 ),
               ),
-            ),
-            TextButton(
-              onPressed: () {
-                print("English");
-              },
-              child: Text(
-                "English",
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.white,
+              TextButton(
+                onPressed: () {
+                  print("English");
+                  Navigator.push(
+                    context,
+                    PageTransition(
+                        child: HomeScreen(),
+                        type: PageTransitionType.leftToRight),
+                  );
+                },
+                child: Text(
+                  "English",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
